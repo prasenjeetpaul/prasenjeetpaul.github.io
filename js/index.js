0ebda3c6b47a8f52dab14373b5f8dbbbd27cc634
Prasenjeet Paul <prasenjeetpaul14@gmail.com>
2021-06-29 23:22:36 +0530

    fix: fixed header box-shadow animation on scroll

--- js/index.js
+++ js/index.js
@@ -18,13 +18,6 @@ ScrollTrigger.create({
     }
 });
 
-ScrollTrigger.create({
-    trigger: 'header',
-    start: 'top top',
-    toggleClass: 'highlight',
-    // markers: true,
-    end: () => `bottom+=${document.body.scrollHeight + 1000}px top`
-});
 
 const introSectionTL = gsap.timeline();
 
@@ -53,7 +46,6 @@ const aboutMeTL = gsap.timeline({
 
 aboutMeTL.from('#about-me-img', { duration: '0.5', opacity: 0, y: 100 });
 aboutMeTL.from('#about-me-intro', { duration: '0.5', opacity: 0, y: 100 }, '-=0.2');
-
 
 
 const whatIDoTL = gsap.timeline({
@@ -233,12 +225,46 @@ function setNavLinkClickListeners() {
     });
 }
 
+function initHeaderAnimation() {
+
+    // The debounce function receives our function as a parameter
+    const debounce = (fn) => {
+        // This holds the requestAnimationFrame reference, so we can cancel it if we wish
+        let frame;
+
+        // The debounce function returns a new function that can receive a variable number of arguments
+        return (...params) => {
+            // If the frame variable has been defined, clear it now, and queue for next frame
+            if (frame) {
+                cancelAnimationFrame(frame);
+            }
+
+            // Queue our function call for the next frame
+            frame = requestAnimationFrame(() => {
+                // Call our function and pass any params we received
+                fn(...params);
+            });
+        };
+    };
+
+    // Reads out the scroll position and stores it in the data attribute
+    // so we can use it in our stylesheets
+    const storeScroll = () => {
+        document.documentElement.dataset.scroll = window.scrollY;
+    };
+
+    // Listen for new scroll events, here we debounce our `storeScroll` function
+    document.addEventListener("scroll", debounce(storeScroll));
+
+    // Update scroll position for first time
+    storeScroll();
+}
+
 function init() {
     initializeNavLinks();
     setNavLinkClickListeners();
     setActiveNavLink('home-nav-link');
+    initHeaderAnimation();
 }
 
 document.onload = init();
-
-
